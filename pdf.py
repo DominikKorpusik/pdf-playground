9369d369d684723d518b93493cb9819dc055eedd
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-08 21:07:59 +0000

    Adding watermark pdf function

--- pdf.py
+++ pdf.py
@@ -4,11 +4,24 @@ import sys
 pdfs = sys.argv[1:]
 
 def pdf_combiner(pdf_lists):
+    '''Combining all PDFs in one pdf'''
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_lists:
         merger.append(pdf)
     merger.write('super.pdf')
 
-pdf_combiner(pdfs)
 
+def pdf_UWatermark(pdf):
+    templet = PyPDF2.PdfFileReader(open(f"{pdf[0]}", "rb"))
+    watermark = PyPDF2.PdfFileReader(open("wtr.pdf", "rb"))
+    output = PyPDF2.PdfFileWriter()
 
+    for i in range(templet.getNumPages()):
+        page = templet.getPage(i)
+        page.mergePage(watermark.getPage(0))
+        output.addPage(page)
+
+        with open("watermarked_doc.pdf", "wb") as pdf:
+            output.write(pdf)
+
+pdf_UWatermark(pdfs)
